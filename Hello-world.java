82a8ea78a51bd4b0d5f4053e4e3f4ec2a800767b
Mohamad Moumneh <mmoumne0@email.cpcc.edu>
2021-03-09 11:14:23 -0500

    Update Hello-world.java

--- Hello-world.java
+++ Hello-world.java
@@ -1,8 +1,9 @@
-public class Hello World
+public class Hello-World
 {
    public static void main(String[] args)
    {
       System.out.println("Hello World.");
+      System.out.println("Program modified, but able to revert to original version using GitHub Desktop,");
    }
 }
 
